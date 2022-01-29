a3fe2bbabdd53258044a4f86dd5df23a8a91a424
ckymichael <michaelcheung1998@gmail.com>
2022-01-29 23:10:51 +0800

    Initial commit

---
+++ src/main/java/com/myproject/whiteapp/WhiteAppApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.whiteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteAppApplication.class, args);
+	}
+
+}
